f016939e258c08219ae8f1046faba156bf7694ca
Aracely <98180186+argw0rk@users.noreply.github.com>
2024-11-15 17:38:45 -0600

    tried to add image pop up
    but i did crop all the images so they all fit well side by side

---
+++ app.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+const sarah = [...document.querySelectorAll('.sarah')];
+
+//popup
+
+const popup = document.querySelector('.popup');
+const closeBtn = document.querySelector(',close-btn');
+const sarah =document.querySelector('.sarah');
+const largeImage = document.querySelector('.large-image');
+const imageInex = document.querySelector('.index');
+const leftArrow = document.querySelector('.left-arrow');
+const rightArrow = document.querySelector('.right-arrow');
+
+let index = 0; //will track our current image
+
+images.forEach((item,i) =>) {
+			   item.addEventListener('click', () =>{
+	popup.classList.toggle('active');
+	
+})
+			   }
